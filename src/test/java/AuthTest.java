ce29de0e945860b42e8409d4b47c5ab0d1f7bb48
Екатерина Зенина <zeninaekaterin@gmail.com>
2022-01-18 15:04:45 +0300

    Initial commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldLoginIfUserRegisteredActive() {
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("active");
+        $("[data-test-id='login'] .input__control").setValue(user.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(user.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[id='root']").shouldBe(exactText("Личный кабинет"));
+    }
+
+    @Test
+    void shouldNotLoginUserIfNonRegisteredActive() {
+        RegistrationInfo user = DataGenerator.Registration.getRandomUser("active");
+        $("[data-test-id='login'] .input__control").setValue(user.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(user.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']").shouldBe(exactText("Ошибка Ошибка! Неверно указан логин или пароль"));
+    }
+    @Test
+    void shouldNotLoginUserIfRegisteredBlocked() {
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("blocked");
+        $("[data-test-id='login'] .input__control").setValue(user.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(user.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']").shouldBe(exactText("Ошибка Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    void shouldNotLoginIfUserRegisteredActiveWrongLogin() {
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("active");
+        String login = DataGenerator.getLogin();
+        $("[data-test-id='login'] .input__control").setValue(login);
+        $("[data-test-id='password'] .input__control").setValue(user.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']").shouldBe(exactText("Ошибка Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldNotLoginIfUserRegisteredActiveWrongPassword() {
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("active");
+        String password = DataGenerator.getPassword();
+        $("[data-test-id='login'] .input__control").setValue(user.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(password);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']").shouldBe(exactText("Ошибка Ошибка! Неверно указан логин или пароль"));
+    }
+}
+
---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,58 @@
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+public class DataGenerator {
+    public static final Faker faker = new Faker(new Locale("en"));
+
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    static void setUpAll(RegistrationInfo user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then();
+    }
+
+    public static String getLogin() {
+        String login = faker.name().username();
+        return login;
+    }
+
+    public static String getPassword() {
+        String password = faker.internet().password();
+        return password;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationInfo getRandomUser(String status) {
+            RegistrationInfo randomUser = new RegistrationInfo(
+                    getLogin(),
+                    getPassword(),
+                    status);
+            return randomUser;
+        }
+
+        public static RegistrationInfo getRegisteredUser(String status) {
+            RegistrationInfo registeredUser = getRandomUser(status);
+            setUpAll(registeredUser);
+            return registeredUser;
+        }
+    }
+}
